15b651021715fabf1d9e870cb29fada0f65727d6
othman93290 <7566@holbertonstudents.com>
2023-10-12 08:59:45 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+int main()
 {
 print ("\"with proper grammar, but the outcome is a piece of art,);
 return (0);
