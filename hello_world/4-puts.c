d542b377d6919a52263a5ee17ce6adb999156d50
othman93290 <7566@holbertonstudents.com>
2023-10-12 08:03:57 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /**
  * La fonction main est le point d'entrée du programme.
  *
- * Cette fonction affiche un message simple à l'écran et renvoie 0 pour indiquer
+ * Cette fonction affiche un message simple à l'écran
+ * et renvoie 0 pour indiquer
  * une exécution réussie du programme.
  *
  * @return 0 en cas de succès.
