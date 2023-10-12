dba22e29a61b5f7b637cc035cbc5c10197cff022
othman93290 <7566@holbertonstudents.com>
2023-10-12 09:21:34 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * La fonction main est le point d'entrée du programme.
+ * Elle affiche un message simple à l'écran.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
