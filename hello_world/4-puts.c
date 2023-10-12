126471adced24190efdf48e0efa1c6c188f369ed
othman93290 <7566@holbertonstudents.com>
2023-10-12 07:58:53 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * La fonction main est le point d'entrée du programme.
+ * Elle affiche un message simple à l'écran.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
