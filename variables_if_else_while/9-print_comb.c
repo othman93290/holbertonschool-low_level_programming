114568cf63421dfdd2619161c6dbf8569574f3a2
othman93290 <7566@holbertonstudents.com>
2023-10-17 06:11:17 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	while (number = '0'; number  <= '9'; number++)
+	putchar (number);
+
+	while (number + '0')
+	putchar (number);
+
+	if (number < '9')
+	{
+		putchar (',');
+		putchar (' ');
+		number++;
+	}
+
+	putchar('\n');
+	return (0);
+}
