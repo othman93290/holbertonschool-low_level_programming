45a0a939f6044ca119d24ab462091e19d2f786e8
othman93290 <7566@holbertonstudents.com>
2023-10-17 14:34:41 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,9 +9,10 @@ int main(void)
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
-		if (number <= '9')
+		putchar(number);
+
+		if (number < '9')
 		{
-	putchar (number);
 	putchar (',');
 	putchar (' ');
 		}
