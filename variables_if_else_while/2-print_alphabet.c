9affa2321c21995c766fc8a7c4e47cf0670fa8b8
othman93290 <7566@holbertonstudents.com>
2023-10-17 02:37:10 -0700

    2-print_alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
-		putchar(letter);
+		putchar (letter);
 		letter++;
 	}
 
