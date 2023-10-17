d2d4aadf006125efd439683e0609facbb04f3c61
othman93290 <7566@holbertonstudents.com>
2023-10-17 02:47:53 -0700

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-        char letter = 'a';
+	char letter = 'a';
 
-        while (letter <= 'z')
-        {
-                putchar (letter);
-                letter++;
-        }
+	while (letter <= 'z')
+	{
+	putchar (letter);
+	letter++;
+	}
 
 	letter = 'A';
 
@@ -25,6 +25,6 @@ int main(void)
 		letter++;
 	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
