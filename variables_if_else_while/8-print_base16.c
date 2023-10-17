b1d30a633977989dbca3b386ef7a9caacb40c4b8
othman93290 <7566@holbertonstudents.com>
2023-10-17 05:03:04 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char number;
+
+	for (number = 0; number < 15; number++)
+	{
+
+        putchar(number);
+	}
+
+        putchar('\n');
+        return (0);
+}
