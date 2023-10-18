c5cbda375a2cde24de33c48bf620f569523a781f
othman93290 <7566@holbertonstudents.com>
2023-10-18 08:19:09 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -17,7 +17,7 @@ void print_alphabet_x10(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-        	for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 
