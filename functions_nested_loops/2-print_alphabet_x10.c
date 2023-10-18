0b64883f39d410dbcd44d0d9a02d56a43fb03e13
othman93290 <7566@holbertonstudents.com>
2023-10-18 08:17:55 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: Always 0 (Success)
  *
@@ -20,7 +20,8 @@ void print_alphabet_x10(void)
         	for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
+
 		}
+		 _putchar('\n');
 	}
-	_putchar('\n');
 }
