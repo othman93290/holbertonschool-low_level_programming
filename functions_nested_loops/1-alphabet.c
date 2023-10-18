07c5c08b1e686a34ce8225efb20ece27b27069d0
othman93290 <7566@holbertonstudents.com>
2023-10-18 06:28:57 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -12,13 +12,13 @@ void print_alphabet(void);
 	int letter = 'a';
 
 	for (letter <= 'z');
-{
+	{
 	putchar (letter);
 	letter++;
-}
-{
+	}
+	{
 	print_alphabet();
-}
+	}
 	putchar("\n");
 	return (0);
 }
