de5eba11d22ec0cb684e9cb9fe0b7634d46570c5
othman93290 <7566@holbertonstudents.com>
2023-10-19 08:13:35 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * print_sign - check if c is print_sign*
+ * @n: The ASCII value of the character to be checked.
+ * Description: check if c is print_sign*
+ * Return: 1 if print_sign or 0 if uppercase
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == '0')
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
