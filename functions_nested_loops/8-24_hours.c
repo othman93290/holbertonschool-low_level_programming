1597fddbadd9e4362af151e668472bf0c3fc5706
othman93290 <7566@holbertonstudents.com>
2023-10-20 10:17:04 -0700

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,15 +3,33 @@
 #include <time.h>
 /**
  * jack_bauer - check if c is jack_bauer*
- * @n: The ASCII value of the character to be checked.
+ * @a, b, c, d: The ASCII value of the character to be checked.
  * Description: check if c is print_sign*
  * Return: 1 if print_sign or 0 if uppercase
  */
 void jack_bauer(void)
-	int a, b, c, d;
-	for a = 
 {
-	return (0);
-}
-	
+	int a, b, c, d;
 
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if ((a <= 2 && (a < 2 || b <= 3)) && c <= 5)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
