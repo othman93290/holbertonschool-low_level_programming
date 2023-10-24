3419ac69b8e87ea853a193501210a59e4ab3c2c6
othman93290 <7566@holbertonstudents.com>
2023-10-24 03:53:42 -0700

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - check if c is _abs*
+ * @a: The ASCII value of the character to be checked.
+ * @b: The ASCII value of the character to be checked.
+ * Description: check if c is print_sign*
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+
+}
