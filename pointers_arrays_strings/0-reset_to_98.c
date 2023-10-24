c56b0c302e16a22a916580f11933f7ee442625c6
othman93290 <7566@holbertonstudents.com>
2023-10-24 03:26:20 -0700

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * @n - variable
- * Return: Always 0.
+ * reset_to_98 - check if c is _abs*
+ * @n: The ASCII value of the character to be checked.
+ * Description: check if c is print_sign*
+ * Return: 1 if print_sign or 0 if uppercase
  */
 void reset_to_98(int *n)
 {
