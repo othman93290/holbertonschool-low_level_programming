7cb205c91c3a9bed6648591702dbff0c292ca930
othman93290 <7566@holbertonstudents.com>
2023-10-26 07:44:20 -0700

    4-print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
+/**
+ * print_rev - Display a string followed by a new line.
+ * @s: The string to be displayed.
+ */
 void print_rev(char *s)
 {
-	int a;
+	int len = strlen(s);
 
-	for (a = 62; (s[a]); a--)
-	{
-	_putchar(s[a]);
-	}
-	_putchar('\n');
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
 }
-
