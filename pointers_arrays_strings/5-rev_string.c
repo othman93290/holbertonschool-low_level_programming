ad547106b962debf8d3af916232a0af30ceda5ab
othman93290 <7566@holbertonstudents.com>
2023-10-27 11:49:44 -0700

    correction

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,28 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _puts - Display a string followed by a new line.
- * @str: The string to be displayed.
+ * rev_string - Reverse a string and display it followed by a new line.
+ * @s: The string to be reversed and displayed.
  */
 void rev_string(char *s)
 {
-	char *begin = s;
-	char *finish = s;
-	char temp;
-
-	while (*finish != '\0')
-	{
-		finish++;
-		finish--;
-	}
-	
-	while (begin < finish)
-	{
-		temp = *begin;
-		*begin = *finish;
-		*finish = temp;
-	begin++;
-	finish--;
-	}
+char *begin = s;
+char *finish = s;
+char temp;
+while (*finish != '\0')
+finish++;
+finish--;
+while (begin < finish)
+{
+temp = *begin;
+*begin = *finish;
+*finish = temp;
+begin++;
+finish--;
 }
-
+}
