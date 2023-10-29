5056f3a3c6dc6e4901b4f20446b86d49769a03fe
othman93290 <7566@holbertonstudents.com>
2023-10-29 12:57:01 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - function that print 1/2 character
+ * @str: input interger
+ * return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int length = strlen(str);
+
+	if (length != 0)
+	{
+		for (i = 0; i < length; i++)
+		{
+			if (i % 2 == 0)
+			{
+				putchar(str[i]);
+			}
+		}
+	}
+	putchar(10);
+}
