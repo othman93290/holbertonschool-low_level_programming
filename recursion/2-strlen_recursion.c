4e2a92dd2d80d9e6309abc454c66818d159a00d8
othman93290 <7566@holbertonstudents.com>
2023-10-31 04:47:10 -0700

    1-print_rev_recursion\2-strlen_recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/*
+ *
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
