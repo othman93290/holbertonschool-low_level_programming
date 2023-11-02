9a024e2c7b1cc29ea9e6c18f692c964a68296a10
othman93290 <7566@holbertonstudents.com>
2023-11-02 01:28:16 -0700

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
- *
- *
+/**
+ * _puts_recursion - a funvtion that concatenate two string
+ * @s: pointer to a char
+ * Return: pointer to a char
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
