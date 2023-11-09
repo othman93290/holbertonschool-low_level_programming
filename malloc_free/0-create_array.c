36ae5f38ff9c91c623b2d340f7569dab9b26be7c
othman93290 <7566@holbertonstudents.com>
2023-11-09 05:44:49 -0800

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - function
+ * @size: unsigned int
+ * @c: char
+ *
+ * Return: char ptr
+ */
+char	*crerate_array(unsigned int size, char c)
+{
+	char *res;
+	unsigned int	x;
+
+	if (size == 0)
+		return (0);
+	res = (char *) malloc(sizeof(char) * size);
+	/**alloue de la memoire / resultat stocker dans res*/
+	if (res == 0)
+		return (0);
+	for (x = 0; x < size; x++)
+		/**Initialise chaque élément du tableau avec c à l'aide d'une boucle for*/
+		res[x] = c;
+	return (res);
+	/**Retourne le pointeur vers le tableau nouvellement créé.*/
+}
