5976776ed4aa1135cf5e9e5d5cc430428cd1075e
othman93290 <7566@holbertonstudents.com>
2023-12-08 06:52:38 -0800

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,33 +1,45 @@
 #include "main.h"
 
 /**
- * append_text_to_file - function
- * @filename: const char ptr
- * @text_content: char ptr
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the text to append to the file
  *
- * Return: int
+ * Return: 1 on success, -1 on failure
  */
-int	append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int	f;
-	int	l;
-	int	x;
+	int fd, write_result;
+	size_t text_length = 0;
 
-	if (filename == 0)
+	/* Check if filename is NULL */
+	if (filename == NULL)
 		return (-1);
-	f = open(filename, O_RDWR | O_APPEND);
-	if (f < 0)
+
+	/* Open the file in append mode, create if doesn't exist */
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-	l = 1;
-	for (x = 0; text_content && text_content[x]; x++)
+
+	/* Check if text_content is NULL */
+	if (text_content != NULL)
 	{
-		l = write(f, &text_content[x], 1);
-		if (l != 1)
-			break;
+		/* Calculate the length of the text_content */
+		while (text_content[text_length] != '\0')
+			text_length++;
+
+		/* Write the text_content to the file */
+		write_result = write(fd, text_content, text_length);
+		if (write_result == -1 || (size_t)write_result != text_length)
+		{
+			/* Error writing to the file */
+			close(fd);
+			return (-1);
+		}
 	}
-	close(f);
-	if (l != 1)
-		return (-1);
+
+	/* Close the file */
+	close(fd);
 	return (1);
 }
 
